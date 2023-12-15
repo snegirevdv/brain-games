c838cb0a389cd8d81e3698192f7ca87fea5bff81
snegirevdv <snegirevdv@gmail.com>
2023-12-15 21:22:57 +0100

    Add module cli

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+from prompt import string
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    username: str = string("May I have your name? ")
+    print(f"Hello, {username}!")
+    return username
