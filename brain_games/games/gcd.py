4a6e9fcd038ae7df1ad52294dd0c7467915f3913
snegirevdv <snegirevdv@gmail.com>
2023-12-20 21:42:53 +0100

    Fix gcd naming

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,11 +7,11 @@ def gen_question_answer() -> tuple[str]:
     number1 = randint(*settings.NUMBER_LIMITS)
     number2 = randint(*settings.NUMBER_LIMITS)
     question = f"{number1} {number2}"
-    correct_answer = str(gcd(number1, number2))
+    correct_answer = str(calc_gcd(number1, number2))
     return question, correct_answer
 
 
-def gcd(a: int, b: int) -> int:
+def calc_gcd(a: int, b: int) -> int:
     while b != 0:
         a, b = b, a % b
     return a
